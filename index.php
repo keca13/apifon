231ed07ba290f6efff0e59ffb08517e126017695
keca13 <keca13@wp.pl>
2018-07-27 15:03:38 +0200

    dodanie adresu uw-team.org

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <script src="main.js"></script>
 </head>
 <body>
-
+<!-- https://www.uw-team.org/-->
 <!-- instrukcje warunkowe i obsluga formularzy-->
 <form method= "post" action"">
 imie: <input type="text" name="imie">
